301e99e590cab1af1fe3edde89f83967ae8560da
Jared Cheng <chengjared6@gmail.com>
2023-09-11 23:01:35 +0100

    added some css, more buttons and get a 25min coundown working

--- contentScript.js
+++ contentScript.js
@@ -1,16 +1,32 @@
-var timer
+const workminutes = 25;
+let time = workminutes * 60 && chillminutes * 60;
 
-var ele = document.getElementById('timer');
 
-(function () {
-    var sec = 0;
-    timer = setInterval(() => {
-        ele.innerHTML = '00:' + sec;
-        sec++;
-    }, 1000) // each 1 second
+const chillminutes = 5;
 
-})()
 
-function pause() {
-    clearInterval(timer);
+const countdownEl = document.getElementById('timer');
+
+
+setInterval(timer, 1000);
+
+function timer() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 5 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
 }
+
+// function timer() {
+//     const minutes = Math.floor(time / 60);
+//     let seconds = time % 60;
+
+//     seconds = seconds < 25 ? '0' + seconds : seconds;
+
+//     countdownEl.innerHTML = `${minutes}: ${seconds}`;
+//     time--;
+// }
+
